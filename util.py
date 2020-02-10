52336f99080f478616a6ec23d8b973f0301d5ad0
hglps <44714747+hglps@users.noreply.github.com>
2020-02-09 23:39:50 -0300

    Update util.py

--- util.py
+++ util.py
@@ -1,10 +1,21 @@
 import pygame
 from screen import *
-from startscreen import *
+# from startscreen import *
+import pandas as pd
 
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+
+
+def actionButton(stateParam=None):
+    print("foi")
+    if stateParam != None:
+        import main
+        main.state = stateParam
+
+        print("agr a main state eh ", stateParam,"  ", main.state)
+
 
 def text_objects(text, font, textColor):
 
@@ -12,42 +23,83 @@ def text_objects(text, font, textColor):
     return textSurface, textSurface.get_rect()
 
 
-def generateButton(msg,x,y,w,h,ic,ac,textColor,fontSize,scr,action=None):
-    '''
+def generateButton(msg, x, y, w, h, ic, ac, textColor, fontSize, scr, action=None, param=None):
+    """
+    :param param: 'action' parameter
+    :param msg: string
+    :param x: int
+    :param y: int
+    :param w: int
+    :param h: int
+    :param ic: tuple
+    :param ac: tuple
     :param textColor: tuple
-    :param fontSize:
-    :param scr: Screen
+    :param fontSize: int
+    :param scr: Screen.getScreen()
     :param action: function
-    :return: void
-    '''
+    :return:
+    """
 
     mouse = pygame.mouse.get_pos()
 
     click = pygame.mouse.get_pressed()
-    #print(click)
+    # print(click)
 
-    if x+w > mouse[0] > x and y+h > mouse[1] > y:
-        pygame.draw.ellipse(scr.getScreen(), ac,(x,y,w,h))
+    if x+w >= mouse[0] >= x and y+h >= mouse[1] >= y:
+        pygame.draw.ellipse(scr, ac,(x,y,w,h))
 
-        if click[0] == 1 and action != None:
-            action()
+        if click[0] == 1:
+            print("clicked") # problem : need to click twice to work ;-;
+            if param != None:
+                action(param)
+                print("foi a action param")
+            else:
+                action()
+            if action == pygame.quit:
+                exit()
     else:
-        pygame.draw.ellipse(scr.getScreen(), ic,(x,y,w,h))
+        pygame.draw.ellipse(scr, ic,(x,y,w,h))
 
     generateText(msg, fontSize, textColor, x+(w/2), y+(h/2), scr)
 
 
 def generateText(text, fontSize, textColor, x, y, scr):
-    '''
-    text: String
-    fontSize : num
-    textColor: tuple
-    x,y : center
-    type: String - "button" or else
-    '''
+    """
+    :param text: string
+    :param fontSize: int
+    :param textColor: tuple
+    :param x: int
+    :param y: int
+    :param scr: Screen.getScreen()
+    :return:
+    """
 
     fontConfig = pygame.font.Font("Akron_Shades_NBP_Regular.ttf", fontSize)
     TextSurf, logo = text_objects(text, fontConfig, textColor)
-    logo.center = (x,y)
+    logo.center = (x, y)
 
-    scr.getScreen().blit(TextSurf, logo)
+    scr.blit(TextSurf, logo)
+
+
+def loadImage(strImage):
+    return pygame.image.load(strImage)
+
+
+def getData():
+    data = pd.read_csv("perguntas.csv")
+    return data
+
+def createBoxCenter():
+    pass
+
+
+def renderQuestionRects(scr):
+    generateButton("1", 150, 120, 100, 80, (12, 191, 117), (17, 150, 124), WHITE, 25, scr, createBoxCenter)
+    generateButton("2", 270, 90, 100, 80, (12, 191, 117), (17, 150, 124), WHITE, 25, scr, createBoxCenter) # 270
+    generateButton("3", 370, 120, 100, 80, (12, 191, 117), (17, 150, 124), WHITE, 25, scr, createBoxCenter)
+    generateButton("4", 470, 90, 100, 80, (12, 191, 117), (17, 150, 124), WHITE, 25, scr, createBoxCenter)
+    generateButton("5", 570, 120, 100, 80, (12, 191, 117), (17, 150, 124), WHITE, 25, scr, createBoxCenter)
+
+    positionButtons = [[150, 250], [270, 370], [370, 470], [470, 570], [570, 670]]
+    return positionButtons
+
