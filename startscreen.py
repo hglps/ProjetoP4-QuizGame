3b8324980b06650b3258e5a046be43d7820a7023
hglps <44714747+hglps@users.noreply.github.com>
2020-02-09 23:40:03 -0300

    Update startscreen.py

--- startscreen.py
+++ startscreen.py
@@ -4,5 +4,21 @@ from util import *
 
 
 class StartScreen(Screen):
-    def __init__(self):
-        self.btnStartGame = button("Start", 400, 150, 100, 60, ())
+    def __init__(self, caption, icon, width, height, backgroundImg):
+        super().__init__(caption, icon, width, height, backgroundImg)
+        self.btnStartGame = generateButton("Start", 570, 120, 130, 60, (12, 191, 117), (17, 150, 124), BLACK, 20,
+                                           super().getScreen(), actionButton, 1)
+        self.btnExit = generateButton("Quit", 570, 200, 130, 60, (12, 191, 117), (17, 150, 124), BLACK, 20,
+                                      super().getScreen(), pygame.quit)
+
+
+####################################################################
+# currentScreen = Screen("Quiz Game", "icon.png", 900, 600)
+
+
+# def testPrint():
+#     print("pressed button")
+#     import main
+#     main.state = 1
+#
+#     # state = 1
